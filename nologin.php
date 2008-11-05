509fd68d25a011cf938584a4f8cb57cd8495ba91
ozh <ozh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-05 16:01:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/no-login/trunk@72292 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nologin.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: .No Login !!
+Plugin URI: http://planetozh.com/
+Description: Never authenticate, you're always the admin. Obviously for test sites!
+Author: Ozh
+Version: 1.00
+Author URI: http://planetozh.com/
+*/
+
+if (!function_exists('wp_validate_auth_cookie')) {
+       function wp_validate_auth_cookie() {
+               return 1;
+       }
+	   wp_enqueue_script('jquery');
+	   add_action('admin_notices', 'ozh_nologin_warn');
+}
+
+function ozh_nologin_warn() {
+	echo '<div class="updated fade" id="ozh_nologin" style="background:#ff0;text-align:center;color: red;"><p><strong>No Login <u>activated</u> - anybody gets admin rights!</strong> <span onclick="javascript:jQuery(this).parent().parent().slideToggle(200)" style="cursor:pointer">[dismiss]</span> </p></div>';
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== No Login ===
+Donate link: http://planetozh.com/exit/donate
+Tags: ozh, login, auth, admin, nologin
+Requires at least: 2.0
+Stable tag: trunk
+
+Skip the login form and always auth as admin. FOR TEST SITES.
+
+== Description ==
+
+Skip the login form: every visitor automatically auths as an admin. FOR 'LOCALHOST' TEST SITES only, obviously, like when designing a plugin or a theme and testing it across various browsers.
+
+== Screenshots ==
+
+1. Everybody is the admin.
+
+== Installation ==
+
+Installation is, as usual :
+
+1. Upload files to your `/wp-content/plugins/` directory (preserve sub-directory structure if applicable)
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Refer to the official plugin page for documentation, usage and tips
