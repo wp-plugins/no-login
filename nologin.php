1100b67694b1929a294c7d65399f0e55e0491daa
ozh <ozh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-13 19:57:27 +0000

    Fix notices
    git-svn-id: https://plugins.svn.wordpress.org/no-login/trunk@892400 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nologin.php
+++ nologin.php
@@ -4,20 +4,24 @@ Plugin Name: .No Login !!
 Plugin URI: http://planetozh.com/blog/my-projects/wordpress-plugin-no-login/
 Description: Never authenticate, you're always the admin. Obviously for test sites!
 Author: Ozh
-Version: 1.0.1
+Version: 1.0.2
 Author URI: http://planetozh.com/
 */
 
+/**/
 if (!function_exists('wp_validate_auth_cookie')) {
        function wp_validate_auth_cookie() {
                return 1;
        }
-	   wp_enqueue_script('jquery');
-	   add_action('admin_notices', 'ozh_nologin_warn');
+	   add_action( 'admin_enqueue_scripts', 'ozh_nologin_enqueue' );
+	   add_action( 'admin_notices', 'ozh_nologin_warn' );
+}
+
+function ozh_nologin_enqueue() {
+    wp_enqueue_script('jquery');
 }
 
 function ozh_nologin_warn() {
 	echo '<div class="updated fade" id="ozh_nologin" style="background:#ff0;text-align:center;color: red;"><p><strong>No Login <u>activated</u> - anybody gets admin rights!</strong> <span onclick="javascript:jQuery(this).parent().parent().slideToggle(200)" style="cursor:pointer">[dismiss]</span> </p></div>';
 }
 
-?>
